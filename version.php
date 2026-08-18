2013bd8a704ce5c0800469ff326ebc5cc1a17834
Josemaria Bolanos <josemabol@gmail.com>
2026-08-18 11:27:22 -0400

    Add release 3.2.6

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026081100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026081800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100700;        // Requires Moodle 4.5+.
 $plugin->component = 'block_dixeo_modulegen';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.2.5';
+$plugin->release   = '3.2.6';
 $plugin->dependencies = [
-    'local_dixeo' => 2026081101,
+    'local_dixeo' => 2026081800,
 ];
