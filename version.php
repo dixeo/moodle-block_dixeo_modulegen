f9a2749a7303dddf163e0512a4c5236cf73a0eea
prismyq <pierre@prismyq.com>
2026-09-18 10:28:01 +0400

    Add release 3.2.8

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026090702;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026091800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100700;        // Requires Moodle 4.5+.
 $plugin->component = 'block_dixeo_modulegen';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.2.7';
+$plugin->release   = '3.2.8';
 $plugin->dependencies = [
-    'local_dixeo' => 2026090700,
+    'local_dixeo' => 2026091800,
 ];
